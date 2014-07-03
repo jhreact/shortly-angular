d00a8a34b6353d3bb723248b6d6beb7086ef60fa
taojonesin <taojonesin (at) nowhere.com>
2014-07-02 17:23:44 -0700

    Added $http get request to getLinks method, added http to dependency injection * test still broken

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,9 +1,16 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, Links, $http) {
   // Your code here
   $scope.data = {};
   $scope.getLinks = function(){
-
+    $http({
+      method: 'GET',
+      url: '/api/links'
+    }).success(function(data, status, headers, config){
+      console.log(data)
+    //   $scope.data.links = data;
+    });
   };
+  $scope.getLinks();
 });

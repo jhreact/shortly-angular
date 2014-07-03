b2cf99ffa4fe9ce0f7ab4616747f81ceb29b099c
jhreact <jhreact@users.noreply.github.com>
2014-07-03 12:02:41 -0700

    correctly posting url and displaying links

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,15 +1,23 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links, $http) {
+  $scope.linkUrl = "";
   $scope.link = {};
+
   $scope.addLink = function(){
+    $scope.link = {url: $scope.linkUrl};
+
     $scope.loading = true;
     $http({
       method: 'POST',
-      url: '/api/links'
+      url: '/api/links',
+      data: JSON.stringify($scope.link)
     }).success(function(data, status, headers, config){
+      console.log("SUCCESS");
+      console.log(data);
       $scope.loading = false;
+    }).fail(function(){
+      console.log('goofd')
     });
-
-  }
+  };
 });

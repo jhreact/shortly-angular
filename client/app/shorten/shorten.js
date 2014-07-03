9165acc9cbd5190aad1ad46904a4c8002358b368
taojonesin <taojonesin (at) nowhere.com>
2014-07-02 19:51:52 -0700

    addlink function stubbed

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,8 +1,15 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location, Links, $http) {
   $scope.link = {};
   $scope.addLink = function(){
+    $scope.loading = true;
+    $http({
+      method: 'POST',
+      url: '/api/links'
+    }).success(function(data, status, headers, config){
+      $scope.loading = false;
+    });
 
   }
 });
